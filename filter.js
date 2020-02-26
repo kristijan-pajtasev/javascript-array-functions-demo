fe0516e39428f25a38db62a58307aa95aad0e4b9
kristijan-pajtasev <kristijan.pajtasev@hotmail.com>
2020-02-26 23:21:27 +0000

    ignore webstorm

---
+++ filter.js
@@ -0,0 +1,4 @@
+// example 1: find all even numbers
+const numbers = [1, 2, 3, 4, 5, 6, 7];
+const evenNumbers = numbers.filter(element => element % 2 === 0);
+console.log(evenNumbers); // [ 2, 4, 6 ]
